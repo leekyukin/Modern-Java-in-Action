b733bf30575e9d6243ae3a1e68146855529c6d2d
leekyukin <leekujin14@gmail.com>
2022-06-19 04:06:46 +0900

    ADD :: 스트림 없이 처리

---
+++ src/com/java/chapter4/Ex1.java
@@ -0,0 +1,29 @@
+package com.java.chapter4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+// Ex2 와 같은 연산에서 스트림을 사용하지 않은 코드
+public class Ex1 {
+    public static void main(String[] args) {
+        List<Dish> menu = Dish.getDishes();
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+
+        for ( Dish dish : menu ) {
+            if (dish.getCalories() < 400)
+                lowCaloricDishes.add(dish);
+        }
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            @Override
+            public int compare(Dish dish1, Dish dish2) {
+                return Integer.compare(dish1.getCalories(), dish2.getCalories());
+            }
+        });
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        for(Dish dish : lowCaloricDishes) {
+            lowCaloricDishesName.add(dish.getName());
+        }
+    }
+}
