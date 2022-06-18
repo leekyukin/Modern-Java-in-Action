cc8571c09ec9338f7d7606b6b3440ac4e486fa67
leekyukin <leekujin14@gmail.com>
2022-06-19 02:25:25 +0900

    ADD :: 앞으로 스트림 예제에 쓰일 클레스

---
+++ src/com/java/chapter4/Dish.java
@@ -0,0 +1,41 @@
+package com.java.chapter4;
+
+// 책에서 스트림 예제에 쓰일 클레스
+public class Dish {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public enum Type { MEAT, FISH, OTHER };
+}
+
